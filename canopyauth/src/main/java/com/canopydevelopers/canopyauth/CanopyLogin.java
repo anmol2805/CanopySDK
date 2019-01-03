edcfca5b515c1acd193a257c67dfd4b3437a30ca
anmol2805 <saxenaanmol28@gmail.com>
2019-01-03 16:36:35 +0000

    started canopyauth

---
+++ canopyauth/src/main/java/com/canopydevelopers/canopyauth/CanopyLogin.java
@@ -0,0 +1,30 @@
+package com.canopydevelopers.canopyauth;
+
+import android.content.Context;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.canopydevelopers.mysingleton.Mysingleton;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class CanopyLogin {
+    public void generate_token(String student_id,
+                                         String password,
+                                         Context context,
+                                         Response.Listener<JSONObject> listener,
+                                         Response.ErrorListener errlsn
+    ){
+        JSONObject credentials = new JSONObject();
+        try {
+            credentials.put("uid",student_id);
+            credentials.put("pwd",password);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        JsonObjectRequest loginRequest = new JsonObjectRequest(Request.Method.POST,
+                "http://14.139.198.171/api/hibi/login_test",
+                credentials, listener, errlsn);
+        Mysingleton.getInstance(context).addToRequestqueue(loginRequest);
+    }
+}
