d0184e7441025d10c2a085900f1e6f3a163173c1
anmol2805 <saxenaanmol28@gmail.com>
2019-01-03 16:50:50 +0000

    testing api

--- app/src/main/java/com/canopydevelopers/canopysdk/MainActivity.kt
+++ app/src/main/java/com/canopydevelopers/canopysdk/MainActivity.kt
@@ -2,11 +2,18 @@ package com.canopydevelopers.canopysdk
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.canopydevelopers.canopyauth.CanopyLogin
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val canopyLogin = CanopyLogin()
+        canopyLogin.generate_token("b516008","anmol@2805","http://14.139.198.171:8080/token/generate-token",this,{response ->
+                System.out.println(response)
+        },{error ->
+                System.out.println(error)
+        })
     }
 }
--- canopyauth/src/main/java/com/canopydevelopers/canopyauth/CanopyLogin.java
+++ canopyauth/src/main/java/com/canopydevelopers/canopyauth/CanopyLogin.java
@@ -11,19 +11,20 @@ import org.json.JSONObject;
 public class CanopyLogin {
     public void generate_token(String student_id,
                                          String password,
+                                         String url,
                                          Context context,
                                          Response.Listener<JSONObject> listener,
                                          Response.ErrorListener errlsn
     ){
         JSONObject credentials = new JSONObject();
         try {
-            credentials.put("uid",student_id);
-            credentials.put("pwd",password);
+            credentials.put("username",student_id);
+            credentials.put("password",password);
         } catch (JSONException e) {
             e.printStackTrace();
         }
         JsonObjectRequest loginRequest = new JsonObjectRequest(Request.Method.POST,
-                "http://14.139.198.171/api/hibi/login_test",
+                url,
                 credentials, listener, errlsn);
         Mysingleton.getInstance(context).addToRequestqueue(loginRequest);
     }
